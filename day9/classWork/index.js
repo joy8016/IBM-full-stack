b8d77ee0f12c79b9fb4a3ba3147845ff0b5074c8
joy8016 <maityjoydeb67@gmail.com>
2025-02-23 21:31:19 +0530

    any massege

---
+++ day9/classWork/index.js
@@ -0,0 +1,32 @@
+// let a = 20, b = "20"
+// console.log(typeof(b))
+// if(a==b){
+//     console.log("a is equal to b")
+
+// }
+// else{
+//     console.log("a is not equal to b")
+// }
+
+//function
+
+//syntex: fun
+//syntex: const myfunction=(parameter)=>{}
+
+// myfunction(a,b)
+
+// let a = 6
+// let b = 8
+
+// const myfunction=(a,b)=>{
+//     console.log(a+b)
+// }
+
+// console.log(name)
+// var name = "joy"
+
+// let d = "10"
+// console.log(d.length()) 
+
+let b = 30, a =+ "20"
+console.log(b+a)
