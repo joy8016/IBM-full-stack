8d6793d4eb053a4b07823755d5a6b624e1b8f12d
joy8016 <maityjoydeb67@gmail.com>
2025-02-24 22:35:40 +0530

    any massege

---
+++ day9/practice/script.js
@@ -0,0 +1,37 @@
+const multiply = (a,b)=>{
+    return a* b;
+}
+    
+
+
+console.log(multiply(4,4))
+
+const myfunction=(a,b)=>{
+    console.log(a+b)
+}
+
+console.log(name)
+var name = "joy"
+
+let d = "10"
+console.log(d.length()) 
+
+let b = 30, a =+ "20"
+console.log(b+a)
+
+
+const obj = {
+    name: "alice",
+    sayhello: function(){
+        setTimeout(()=>{
+            console.log("hello," + this.name);
+        }
+        ,1
+
+    );
+    
+    }
+};
+
+obj.sayhello();
+
